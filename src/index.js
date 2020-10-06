a233f524fc9b30ab75daccb780c23778232ce3f7
Anton <toshi4ka7@mail.ru>
2020-10-06 22:02:36 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n += '';
+    n = n.split('').reverse().join('');
+    console.log(n[n.length - 1]);
+    if (n[n.length - 1] === '-') {
+      n = n.substring(0, n.length - 1);
+    }
+    return +n;
 }
